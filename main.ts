a7ca1a3bfa6b13044bc0f7431ced46343dfb1fc8
Геннадий Покидов <g.pokidov@cft.ru>
2021-07-01 18:37:21 +0700

    sumPairs

--- main.ts
+++ main.ts
@@ -9,24 +9,54 @@ export class Main {
         // 32   252 / 6 = 42
         // 64   924 / 7 = 132
         // 128   3432 / 8 = 429
+        console.log(this.sumPairs([1, 4, 8, 7, 3, 15], 8))
+        console.log(this.sumPairs([1, -2, 3, 0, -6, 1], -6))
+        console.log(this.sumPairs([20, -13, 40], -7))
+        console.log(this.sumPairs([1, 2, 3, 4, 1, 0], 2))
+        console.log(this.sumPairs([10, 5, 2, 3, 7, 5], 10))
+        console.log(this.sumPairs([4, -2, 3, 3, 4], 8))
+        console.log(this.sumPairs([0, 2, 0], 0))
+        console.log(this.sumPairs([5, 9, 13, -3], 10))
 
-        this.easyLine(2)
-        this.easyLine(3)
-        this.easyLine(4)
-        this.easyLine(5)
-        this.easyLine(6)
-        this.easyLine(7)
-        this.easyLine(8)
+    }
+
+    private sumPairs(ints: number[], s: number) {
+        let arr: number[] = [];
+        for (let i = 0; i < ints.length; i++) {
+            if (arr.length) {
+                break;
+            }
+            ints.reduce((t, c, ind, a) => {
+                if (ind + i < a.length && !arr.length && t + a[ind + i] === s) {
+                    arr.push(t, a[ind + i])
+                }
+                return c
+            })
+        }
+        return arr.length ? arr : undefined;
     }
 
     private easyLine(n: number) {
-        const factorial: any = (num: any) => num ? num * factorial(num - 1) : 1;
-        let result: number[] = [];
-        for (let i = 0; i <= n; i++) {
-            result.push(factorial(n) / (factorial(i) * factorial(n - i)))
+        const binCoef = (a: number, b: number) => {
+            b = Math.min(b, a - b);
+            let c: number = 1;
+            for (let i = 0; i < b; i++) {
+                c *= (a - i);
+                c /= i + 1;
+            }
+            return c;
         }
-        console.log((result.reduce((t, c) => t + (c ** 2), 0)))
+        return binCoef(n * 2, n);
     }
+
+    // private easyLine(n: number) {
+    //     const factorial: any = (num: any) => num ? num * factorial(num - 1) : 1;
+    //     let result: number[] = [];
+    //     for (let i = 0; i <= n; i++) {
+    //         result.push(factorial(n) / (factorial(i) * factorial(n - i)))
+    //     }
+    //     console.log((result.reduce((t, c) => t + (c ** 2), 0)))
+    // }
 
     private isSatorSquare(tablet: string[][]) {
         const hor = tablet.flat().join('');
@@ -37,15 +67,6 @@ export class Main {
         }, []);
         const vertR = vert.reverse().join('');
         return [hor, horR, vert.join(''), vertR].every((d) => d === hor);
-    }
-
-    private sumPairs(ints: number[], s: number) {
-        ints.reduce((t, c, i, a) => {
-            console.log(t, c)
-            return t
-        }, [])
-        console.log()
-        return [0, 0] || undefined;
     }
 
     private findSquares(num: number) {
